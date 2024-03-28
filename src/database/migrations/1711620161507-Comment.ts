6643d42e052e15b03a9f13dda3dbd40c30373262
eunchong2lee <tlfqjrjs1234@naver.com>
2024-03-28 19:03:41 +0900

    Fix: Migrations

--- src/database/migrations/1711620161507-Comment.ts
+++ src/database/migrations/1711620161507-Comment.ts
@@ -1,15 +1,15 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class BoardIndexADD1711607435316 implements MigrationInterface {
+export class CommentMigrationADD1711607435316 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      CREATE INDEX idx_title ON board(title);
+
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      DROP INDEX idx_title;
+
     `);
   }
 }
